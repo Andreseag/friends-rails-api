5c0cdc669b702f940ff79f0399eed957ae811dea
Andres Castro <castrogomezand@gmail.com>
2023-05-07 12:24:57 -0500

    feat: add create friend endpoint :sparkles:

--- app/controllers/api/v1/friends_controller.rb
+++ app/controllers/api/v1/friends_controller.rb
@@ -8,4 +8,21 @@ class Api::V1::FriendsController < ApplicationController
       render json: friends.errors, status: :bad_request
     end
   end
+
+  def create 
+    friend = Friend.new(friend_params)
+
+    if friend.save
+      render json: { status: "SUCCESS", message: "Friend was created successfully!", data: friend }, status: :created
+    else 
+      render json: friend.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def friend_params 
+    params.required(:friend).permit(:name, :location, :email, :twitter, :phone)  
+  end
+  
 end
